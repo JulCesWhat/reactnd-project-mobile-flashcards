4040e3c6574d505104a4f0e4c6fcd212a5fe0432
Whatley, Cesar <julceswhat@gmail.com>
2020-07-19 17:00:18 -0400

    Adding necessary files to start project

---
+++ utils/api.js
@@ -0,0 +1,66 @@
+import { AsyncStorage } from 'react-native';
+
+export const MOBILE_FLASHCARDS = 'Mobile:flashcards'
+
+const startingContent = {
+    React: {
+        title: 'React',
+        questions: [
+            {
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces'
+            },
+            {
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event'
+            }
+        ]
+    },
+    JavaScript: {
+        title: 'JavaScript',
+        questions: [
+            {
+                question: 'What is a closure?',
+                answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+        ]
+    }
+}
+
+export function getDecks() {
+    return AsyncStorage.getItem(MOBILE_FLASHCARDS)
+        .then((res) => {
+            if (res) {
+                return res;
+            } else {
+                AsyncStorage.mergeItem(MOBILE_FLASHCARDS, JSON.stringify(startingContent));
+                return startingContent;
+            }
+        }).catch((err) => {
+            console.log('There was an error: ' + err);
+        });
+}
+
+export function getDeck(id) {
+    return AsyncStorage.getItem(MOBILE_FLASHCARDS)
+        .then((results) => {
+            const data = JSON.parse(results);
+            return data[id];
+        });
+}
+
+export function saveDeckTitle(title) {
+    let newTile = title.toLowerCase();
+    newTile = newTile.replace(/\s+/g, '')
+    return AsyncStorage.mergeItem(MOBILE_FLASHCARDS, JSON.stringify({
+        [newTile]: {}
+    }));
+}
+
+export function addCardToDeck(title, card) {
+    let newTile = title.toLowerCase();
+    newTile = newTile.replace(/\s+/g, '')
+    return AsyncStorage.mergeItem(MOBILE_FLASHCARDS, JSON.stringify({
+        [newTile]: card
+    }));
+}
